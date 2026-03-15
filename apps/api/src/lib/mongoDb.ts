2211eaac7fc0d40d005de81216d172433397620c
DrAmanJy <dramanjy0770@gmail.com>
2026-03-15 23:31:33 +0530

    fix(api): update MongoDB connection URI environment variables for consistency

--- apps/api/src/lib/mongoDb.ts
+++ apps/api/src/lib/mongoDb.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export default async function connectDb() {
   try {
     const conn = await mongoose.connect(
-      `${process.env.MONGO_URI}/${process.env.DB_NAME}`,
+      `${process.env.MONGODB_URI}/${process.env.MONGODB_DB_NAME}`,
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
